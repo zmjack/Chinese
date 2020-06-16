ccf1b5d874039df94d01d4787ab1d818619b5c09
zmjack <jack@zmland.com>
2020-06-16 18:25:16 +0800

    v0.0.9

--- Chinese/ChineseConverter.cs
+++ Chinese/ChineseConverter.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
+﻿
 namespace Chinese
 {
     using Converter = Microsoft.International.Converters.TraditionalChineseToSimplifiedConverter.ChineseConverter;
