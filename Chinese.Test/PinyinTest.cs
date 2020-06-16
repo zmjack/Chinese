2159e8a989455cab076d038ccfd204062e3efb87
zmjack <jack@zmland.com>
2020-06-16 17:12:22 +0800

    v0.0.8

--- Chinese.Test/PinyinTest.cs
+++ Chinese.Test/PinyinTest.cs
@@ -7,7 +7,6 @@ namespace Chinese.Test
         [Fact]
         public void Test1()
         {
-            var s = Pinyin.GetString("的", PinyinFormat.PhoneticSymbol);
             Assert.Equal("mian3 fei4，kua4 ping2 tai1，kai1 yuan2！", Pinyin.GetString("免费，跨平台，开源！", PinyinFormat.Default));
             Assert.Equal("mian fei，kua ping tai，kai yuan！", Pinyin.GetString("免费，跨平台，开源！", PinyinFormat.WithoutTone));
             Assert.Equal("miǎn fèi，kuà píng tāi，kāi yuán！", Pinyin.GetString("免费，跨平台，开源！", PinyinFormat.PhoneticSymbol));
